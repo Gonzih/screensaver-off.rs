b3a9938bddbe09abe2238b4031e1f5265a8ffc0b
Max Gonzih <gonzih@gmail.com>
2020-01-16 18:13:33 -0500

    Add rustup

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {};
 pkgs.mkShell {
   buildInputs = [
-    glib pkgconfig openssl cmake pango atk gdk-pixbuf gtk3
+    glib pkgconfig openssl cmake pango atk gdk-pixbuf gtk3 rustup
   ];
 }
